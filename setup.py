2892c086493a6c69c7a621d0e88ff96c5f2503a7
Kashif Iftikhar <kashif@compulife.com.pk>
2010-12-14 14:10:40 +0500

    Moved all modules to pyutils folders so a package is created. Now instead of the modules being accessible directly for import; are imported using pyutils.db etc.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 setup(name='pyutils',
-      version='0.4',
+      version='0.4.1',
       summary='Python utility modules',
-      py_modules=['datetime_utils', 'db', 'debug', 'sorting', 'type_conversion', 'google_search', 'csv_parser', 'html_parser', 'json2csv'],
+      py_modules=['pyutils.datetime_utils', 'pyutils.db', 'pyutils.debug', 'pyutils.sorting', 'pyutils.type_conversion', 'pyutils.google_search', 'pyutils.csv_parser', 'pyutils.html_parser', 'pyutils.json2csv'],
       author='Kashif Iftikhar <kashif@compulife.com.pk>',
       keywords='Python utility db datetime json google csv',
       url='http://www.compulife.com.pk/oss/pyutils'
